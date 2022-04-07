f2b6c8ea40b3ccea2365434d3c5422ae16082324
qiang.ding <qiang.ding@ly.com>
2022-04-07 17:24:15 +0800

    add cmp-bottom-modal component

---
+++ miniprogram/components/cmp-bottom-modal/cmp-bottom-modal.js
@@ -0,0 +1,27 @@
+// components/cmp-bottom-modal/cmp-bottom-modal.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  options: {
+    styleIsolation: 'apply-shared'
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
